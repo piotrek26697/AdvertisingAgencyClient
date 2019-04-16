379391427a2457b5ad21f32fc764ed4fdc457a60
Piotr Musioł (piotmus099) <piotmus099@student.polsl.pl>
2019-04-16 16:59:14 +0200

    Entities extended

--- src/main/model/entities/Advertisement.java
+++ src/main/model/entities/Advertisement.java
@@ -20,4 +20,74 @@ public class Advertisement
     private Invoice invoice;
 
     private List<Billboard> billboardList;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public Calendar getDateFrom()
+    {
+        return dateFrom;
+    }
+
+    public void setDateFrom(Calendar dateFrom)
+    {
+        this.dateFrom = dateFrom;
+    }
+
+    public Calendar getDateTo()
+    {
+        return dateTo;
+    }
+
+    public void setDateTo(Calendar dateTo)
+    {
+        this.dateTo = dateTo;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public Client getClient()
+    {
+        return client;
+    }
+
+    public void setClient(Client client)
+    {
+        this.client = client;
+    }
+
+    public Invoice getInvoice()
+    {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice)
+    {
+        this.invoice = invoice;
+    }
+
+    public List<Billboard> getBillboardList()
+    {
+        return billboardList;
+    }
+
+    public void setBillboardList(List<Billboard> billboardList)
+    {
+        this.billboardList = billboardList;
+    }
 }
--- src/main/model/entities/Billboard.java
+++ src/main/model/entities/Billboard.java
@@ -7,9 +7,51 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Billboard
 {
+    private int id;
+
     private String address;
 
     private Advertisement advertisementDisplayed;
 
     private BillboardSize billboardSize;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public Advertisement getAdvertisementDisplayed()
+    {
+        return advertisementDisplayed;
+    }
+
+    public void setAdvertisementDisplayed(Advertisement advertisementDisplayed)
+    {
+        this.advertisementDisplayed = advertisementDisplayed;
+    }
+
+    public BillboardSize getBillboardSize()
+    {
+        return billboardSize;
+    }
+
+    public void setBillboardSize(BillboardSize billboardSize)
+    {
+        this.billboardSize = billboardSize;
+    }
 }
